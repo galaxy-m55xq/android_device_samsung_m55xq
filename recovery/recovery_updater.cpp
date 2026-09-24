f1a305e085517e671bb89bc48fb1ef53625c48a6
Jamie Falconer <savedbylight25@proton.me>
2026-09-24 09:02:12 +0100

    Refactor recovery updater license and registration
    Updated license header and simplified registration function.

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -1,24 +1,11 @@
 /*
  * Copyright (C) 2021-2026 The LineageOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <edify/expr.h>
-
-// Empty registration is enough to satisfy the build.
+// Minimal registration function required by the build system.
 // Add custom edify functions here later if needed.
-void Register_librecovery_updater_samsung_sm7450() {
-    // Example of how you would register a function:
-    // RegisterFunction("samsung.verify_bootloader", VerifyBootloaderFn);
+extern "C" void Register_librecovery_updater_samsung_sm7450() {
+    // Empty is fine for most modern devices.
 }
